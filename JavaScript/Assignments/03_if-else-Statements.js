0453140ca56d9f3b106bd6febc7330597abe19ea
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-09 20:02:43 +0530

    if-else

---
+++ JavaScript/Assignments/03_if-else-Statements.js
@@ -0,0 +1,13 @@
+//Eligible OR NotEligible For Voting.
+let age = 18;
+
+if (age >= 18) 
+ {
+    console.log("You are Eligible to vote.");
+ } 
+else
+{
+    console.log("You are not Eligible to vote.");
+}
+    
+
